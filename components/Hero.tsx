d2132ec9817a0b4cd1a9de93b4f01dec7dee4bf3
ahmed thousif <ahmedthousif571@gmail.com>
2024-06-09 01:21:41 +0530

    herosection done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -26,6 +29,15 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify">
          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
+
+         <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl" words="Transforming concepts into seamless experiences" />
+         <p className="text-center">
+           Hi, I&apos;m Ahmed Thousif, a MERN stack Developer based in India
+         </p>
+
+         <a href="#about">
+          <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right"/> 
+         </a>
         </div>
 
       </div>
